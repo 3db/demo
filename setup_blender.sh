c4cc4dcbc692d55185253442b2e0a54722c2ed2e
Guillaume Leclerc <leclerc@mit.edu>
2021-04-13 23:09:03 -0400

    Fix blender installation

--- setup_blender.sh
+++ setup_blender.sh
@@ -1,6 +1,8 @@
-#/bin/bash
+#!/bin/bash
 
 curl "https://mirror.clarkson.edu/blender/release/Blender2.92/blender-2.92.0-linux64.tar.xz" --output /tmp/blender.tar.xz
 tar -xf /tmp/blender.tar.xz -C ./
+rm /tmp/blender.tar.xz
 mv blender-2.92.0-linux64/ blender
-
+rm -rf ./blender/2.92/python
+ln -s $CONDA_PREFIX ./blender/2.92/python
